d7e0aa81662d143265e5ec341ea35df077b73c0f
Ian Ctn <55019@etu.he2b.be>
2022-12-20 18:46:27 +0100

    algo de theme TODO and google map

--- app/src/main/java/mobg5/g55019/mobg5_project/informations/BeerInformationFragment.kt
+++ app/src/main/java/mobg5/g55019/mobg5_project/informations/BeerInformationFragment.kt
@@ -1,16 +1,27 @@
 package mobg5.g55019.mobg5_project.informations
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import mobg5.g55019.mobg5_project.R
 import mobg5.g55019.mobg5_project.databinding.FragmentBeerInformationBinding
 import mobg5.g55019.mobg5_project.model.Beer
+import mobg5.g55019.mobg5_project.model.ColorModel
+
 
 //TODO : debug la navigation depuis cette page FIXED
 //C'est comme si le fragment était attaché à favouriteFragment
@@ -46,7 +57,8 @@ class BeerInformationFragment : Fragment() {
     }
 
 
-    private fun setUpView(){
+    private fun setUpView() {
+        // Image Loading
         Glide.with(this).load(currentBeer.imageUrl)
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .override(400, 400)
@@ -54,9 +66,97 @@ class BeerInformationFragment : Fragment() {
 
         binding.beername.text = currentBeer.name
         binding.longDescTV.text = currentBeer.longDescription
-        binding.typeTV.text = binding.typeTV.text.toString().plus(currentBeer.type)
-        binding.colorTV.text = binding.colorTV.text.toString().plus(currentBeer.color)
-        binding.countryTV.text = binding.countryTV.text.toString().plus(currentBeer.country)
-        binding.breweryTV.text = binding.breweryTV.text.toString().plus(currentBeer.brewery)
+
+        // Liste des TextViews à mettre à jour
+        val textViews = listOf(binding.typeTV, binding.colorTV, binding.countryTV, binding.breweryTV)
+
+        // Fonction pour mettre à jour le texte d'un TextView
+        fun updateTextView(textView: TextView, text: String) {
+            val spannable = SpannableString(textView.text.toString().plus(text))
+            val colonPosition = spannable.indexOf(":")
+            spannable.setSpan(StyleSpan(Typeface.BOLD), 0, colonPosition + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            // Mettre à jour la couleur du texte si nécessaire
+            if (textView == binding.colorTV) {
+                val color = colorFinder(currentBeer.color)
+                val colorSpan = ForegroundColorSpan(Color.parseColor(color))
+                spannable.setSpan(colorSpan, spannable.length - currentBeer.color.length,
+                    spannable.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            }
+
+            textView.text = spannable
+        }
+
+        // Itérer sur chaque TextView et mettre à jour le texte
+        for (textView in textViews) {
+            when (textView) {
+                binding.typeTV -> updateTextView(textView, currentBeer.type)
+                binding.colorTV -> updateTextView(textView, currentBeer.color)
+                binding.countryTV -> updateTextView(textView, currentBeer.country)
+                binding.breweryTV -> updateTextView(textView, currentBeer.brewery)
+            }
+        }
+    }
+
+
+    /*
+    private fun setUpView(){
+        var colonPosition = 0
+
+        //Image Loading
+        Glide.with(this).load(currentBeer.imageUrl)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .override(400, 400)
+            .into(binding.beerImage)
+
+        binding.beername.text = currentBeer.name
+        binding.longDescTV.text = currentBeer.longDescription
+
+        var spannable = SpannableString(binding.typeTV.text.toString().plus(currentBeer.type))
+        colonPosition = spannable.indexOf(":")
+        spannable.setSpan(StyleSpan(Typeface.BOLD), 0, colonPosition + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.typeTV.text = spannable
+
+        //Color for the type of color
+        spannable = SpannableString(binding.colorTV.text.toString().plus(currentBeer.color))
+        val color = colorFinder(currentBeer.color)
+        val colorSpan = ForegroundColorSpan(Color.parseColor(color))
+        colonPosition = spannable.indexOf(":")
+        spannable.setSpan(colorSpan, spannable.length - currentBeer.color.length,
+            spannable.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannable.setSpan(StyleSpan(Typeface.BOLD), 0, colonPosition + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.colorTV.text = spannable
+
+        spannable = SpannableString(binding.countryTV.text.toString().plus(currentBeer.country))
+        colonPosition = spannable.indexOf(":")
+        spannable.setSpan(StyleSpan(Typeface.BOLD), 0, colonPosition + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.countryTV.text = spannable
+
+        spannable = SpannableString(binding.breweryTV.text.toString().plus(currentBeer.brewery))
+        colonPosition = spannable.indexOf(":")
+        spannable.setSpan(StyleSpan(Typeface.BOLD), 0, colonPosition + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.breweryTV.text = spannable
+    }
+    */
+
+
+    private fun colorFinder(color :String): String{
+        return when (color.uppercase()) {
+            ColorModel.BLONDE.name -> ColorModel.BLONDE.hex
+            ColorModel.BRUNE.name -> ColorModel.BRUNE.hex
+            ColorModel.ROUSSE.name -> ColorModel.ROUSSE.hex
+            ColorModel.AMBREE.name -> ColorModel.AMBREE.hex
+            ColorModel.DOREE.name -> ColorModel.DOREE.hex
+            ColorModel.CUIVREE.name -> ColorModel.CUIVREE.hex
+            ColorModel.NOIRE.name -> ColorModel.NOIRE.hex
+            ColorModel.VERTE.name -> ColorModel.VERTE.hex
+            ColorModel.BLANCHE.name -> ColorModel.BLANCHE.hex
+            ColorModel.ROUGE.name -> ColorModel.ROUGE.hex
+            ColorModel.ORANGE.name -> ColorModel.ORANGE.hex
+            ColorModel.JAUNE.name -> ColorModel.JAUNE.hex
+            ColorModel.VIOLETTE.name -> ColorModel.VIOLETTE.hex
+            else -> ColorModel.DEFAULT.hex
+        }
+
     }
 }
---
+++ app/src/main/java/mobg5/g55019/mobg5_project/model/ColorModel.kt
@@ -0,0 +1,18 @@
+package mobg5.g55019.mobg5_project.model
+
+enum class ColorModel(val hex: String) {
+    BLONDE("#dbc7a0"),
+    BRUNE("#7d3a15"),
+    ROUSSE("#ad4f09"),
+    AMBREE("#FFBF00"),
+    DOREE("#FFD700"),
+    CUIVREE("#B87333"),
+    NOIRE("#000000"),
+    VERTE("#00FF00"),
+    BLANCHE("#FFFFFF"),
+    ROUGE("#EB4034"),
+    ORANGE("#FFA500"),
+    JAUNE("#FFFF00"),
+    VIOLETTE("#EE82EE"),
+    DEFAULT("#000000")
+}
